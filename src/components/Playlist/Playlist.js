d8210d517678ed56aae7c2cff3714ef7ad3fccd3
cheow ivy <ivycheow86@gmail.com>
2024-01-09 23:44:15 +0800

    update useState for playlistname

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -17,7 +17,7 @@ const Playlist = ( {onNameChange, playlistTracks, onRemove, onSave} ) => {
   return (
     <div className="Playlist">
         {/* for users to input name of the platlist */}
-        <input onChange={handleNameChange} value={playlistName}></input>
+        <input onChange={handleNameChange} value={playlistName === "New Playlist" ? "" : playlistName}></input>
         <TrackList 
             // contains the list of tracks to be displayed
             tracks={playlistTracks}
